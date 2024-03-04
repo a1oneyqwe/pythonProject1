cc9ebd2399c2b97c058489fda0a59c87fd401560
Illia Martynov <s29076@pjwstk.edu.pl>
2024-03-04 14:34:55 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+surname = input("What is your surname? ")
+age = input("How old are you? ")
+sentence = f"Hello, {name} {surname}! You are {age} years old."
+print(sentence)
